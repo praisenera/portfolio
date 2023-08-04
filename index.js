ebc08cf891f1c15d1ffe31796b1921923f0dff24
praisenera <praisenera.02@gmail.com>
2023-08-04 20:18:05 +0800

    Adding images

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-  $(".btn").mouseenter(function () {
-    $("this").css("background": "")
+  let currentUrlPage = window.location.href;
+
+  $(".nav a").each(function () {
+    let linkUrl = $(this).attr("href");
+
+    if (currentPageUrl.indexOf(linkUrl) !== -1) {
+      $(this).addClass("active");
+    }
   });
 });
